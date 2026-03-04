2216ac5ec0b3ff5fabdcebd10c6ff6118bd07721
felipermelo7-blip <felipe.r.melo7@aluno.senai.br>
2026-03-04 14:46:07 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.json({msg: "Hello World"})
+})
+
+app.listen(5000, ()=>{
+    console.log('servidor rodando http://localhost:5000')
+})
+
